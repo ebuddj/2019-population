6581360f5b99ea5a84d369306f8d1198e54a2e51
Teemo Tebest <teemo.tebest@gmail.com>
2019-07-08 14:22:40 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,8 @@
+# 2019-population
+
+https://ebuddj.github.io/2019-population/
+
+## How to use
+
+`npm install`
+`npm start`
---
+++ src/jsx/App.jsx
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import style from './../styles/styles.less';
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+  componentDidMount() {
+
+  }
+  componentWillUnMount() {
+
+  }
+  componentWillReceiveProps(props) {
+
+  }
+  render() {
+    return (
+      <div>Build your application</div>
+    );
+  }
+}
+export default App;
